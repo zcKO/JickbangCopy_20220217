05b742c54665ed8ad50ad70be080311bb25addee
JaeCheon <studyJcHub@gmail.com>
2022-02-17 15:08:30 +0900

    룸 정보 클래스와 기본 어댑터 getView()

---
+++ app/src/main/java/com/jc/jickbangcopy_20220217/adapters/RoomAdapter.kt
@@ -0,0 +1,31 @@
+package com.jc.jickbangcopy_20220217.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.jc.jickbangcopy_20220217.R
+import com.jc.jickbangcopy_20220217.datas.RoomData
+
+class RoomAdapter(
+    val mContext: Context,
+    resId: Int,
+    val roomList: ArrayList<RoomData>
+): ArrayAdapter<RoomData>(mContext, resId, roomList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
+}
---
+++ app/src/main/java/com/jc/jickbangcopy_20220217/datas/RoomData.kt
@@ -0,0 +1,14 @@
+package com.jc.jickbangcopy_20220217.datas
+
+/**
+ * @param price 방 가격
+ * @param address 방 주소
+ * @param floor 층 수
+ * @param description 설명 문구
+ */
+class RoomData(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String
+)
