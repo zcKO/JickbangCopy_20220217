959f64020bd98d3052dc8a9830da244156fe2817
JaeCheon <studyJcHub@gmail.com>
2022-02-17 17:29:19 +0900

    RoomData 클래스를 통째로 첨부 (putExtra) 내방식대로 해보기

--- app/src/main/java/com/jc/jickbangcopy_20220217/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/jc/jickbangcopy_20220217/ViewRoomDetailActivity.kt
@@ -2,6 +2,7 @@ package com.jc.jickbangcopy_20220217
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jc.jickbangcopy_20220217.datas.RoomData
 import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
@@ -9,8 +10,15 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
 
-        val getPrice = intent.getStringExtra("price")
-        txtPrice.text = getPrice
+        val roomData = intent.getSerializableExtra("roomInfo")
+
+        if (roomData is RoomData) {
+
+            txtPrice.text = roomData.getFormattedPrice()
+            txtDescription.text = roomData.description
+            txtAddress.text = roomData.address
+            txtFloor.text = roomData.getFormattedFloor()
+        }
 
     }
 }
